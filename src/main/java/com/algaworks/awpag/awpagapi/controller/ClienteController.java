a0f536ecd1c1bed9aec9a60f571f70446a1dcfcf
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-03-22 20:31:49 -0300

    Usando Jakarta Persistence

--- src/main/java/com/algaworks/awpag/awpagapi/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/awpagapi/controller/ClienteController.java
@@ -1,30 +1,24 @@
 package com.algaworks.awpag.awpagapi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.awpag.model.ClienteModel;
+import com.algaworks.awpag.model.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @RestController
 public class ClienteController {
-    
+
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
-    public List<ClienteModel> lista() {
-        var cliente1 = new ClienteModel();
-        cliente1.setId(1L);
-        cliente1.setNome("João da silva correia");
-        cliente1.setEmail("joaodascouves@gmail.com");
-        cliente1.setTelefone("44 3425 2978");
-
-        var cliente2 = new ClienteModel();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria");
-        cliente2.setEmail("mariadasilva@hotmail.com");
-        cliente2.setTelefone("44 3425 2973");
-
-        return Arrays.asList(cliente1, cliente2);
+    public List<Cliente> lista() {
+        return manager.createQuery("from Cliente", Cliente.class)
+            .getResultList();
     }
 }
---
+++ src/main/java/com/algaworks/awpag/model/Cliente.java
@@ -0,0 +1,27 @@
+package com.algaworks.awpag.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "cliente")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+public class Cliente {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+
+    @Column(name = "fone")
+    private String telefone;
+}
